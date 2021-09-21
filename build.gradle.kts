2c5598aed8c2bc80d766965b11fdec755458e059
darkweird <foxflameless@gmail.com>
2021-09-21 16:23:49 +0300

    tests: implements basic mock tests for w3c

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,10 +21,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
     implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
-    implementation("io.ktor:ktor-client-okhttp:$ktor_version")
     implementation("io.ktor:ktor-client-serialization:$ktor_version")
 
     testImplementation("io.kotest:kotest-runner-junit5:4.6.2")
+    testImplementation("io.ktor:ktor-client-mock:$ktor_version")
 }
 
 
