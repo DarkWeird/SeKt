b0fe52f6390ab565cc436b9f92cfe2509b1c22d3
darkweird <foxflameless@gmail.com>
2022-08-24 17:43:31 +0300

    update browserstack.kt to 0.3.1

--- browserstack/build.gradle.kts
+++ browserstack/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     implementation(project(":core"))
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 
-    api("io.github.darkweird:browserstack.kt:0.3.0")
+    api("io.github.darkweird:browserstack.kt:0.3.1")
     implementation("io.ktor:ktor-client-auth-jvm:$ktor_version")
     implementation("io.ktor:ktor-client-serialization-jvm:$ktor_version")
 }
